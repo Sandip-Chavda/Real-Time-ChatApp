c6b977f81a6017e4c508d7d0fb47c3f15cb61b06
Sandip-Chavda <chavdas766@gmail.com>
2024-04-11 12:45:08 +0530

    Complete Project

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -36,11 +36,11 @@ export const sendMessage = async (req, res) => {
 
     // // SOCKET IO FUNCTIONALITY WILL GO HERE
 
-    // const receiverSocketId = getReceiverSocketId(receiverId);
-    // if (receiverSocketId) {
-    //   // io.to(<socket_id>).emit() used to send events to specific client
-    //   io.to(receiverSocketId).emit("newMessage", newMessage);
-    // }
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,25 +8,28 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   // root route
-//   res.send("Server is ready by sandipsdjkdskjdj ");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.listen(PORT, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server Running on port ${PORT}`);
 });

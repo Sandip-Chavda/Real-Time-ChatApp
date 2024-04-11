fd744265a4d41f782d19a561cd97a58537a6d355
Sandip-Chavda <chavdas766@gmail.com>
2024-04-11 09:14:31 +0530

    Bakend Completed

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// const express = require("express");
+// const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route
+//   res.send("Server is ready by sandipsdjkdskjdj ");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
